274ed4885a7b6a36e065e6624e113c405c1ac91c
Amy Huang <a3315520@gmail.com>
2019-05-29 14:42:26 +0800

    fix equal problem

--- src/components/making/MakingCollection.js
+++ src/components/making/MakingCollection.js
@@ -270,17 +270,17 @@ class MakingCollection extends React.Component {
                 if (view.getUint16(offset + 2, false) <= 8) return callback(-1);
                 let marker = view.getUint16(offset, false);
                 offset += 2;
-                if (marker == 0xFFE1) {
-                    if (view.getUint32(offset += 2, false) != 0x45786966) {
+                if (marker === 0xFFE1) {
+                    if (view.getUint32(offset += 2, false) !== 0x45786966) {
                         return callback(-1);
                     }
 
-                    let little = view.getUint16(offset += 6, false) == 0x4949;
+                    let little = view.getUint16(offset += 6, false) === 0x4949;
                     offset += view.getUint32(offset + 4, little);
                     let tags = view.getUint16(offset, little);
                     offset += 2;
                     for (let i = 0; i < tags; i++) {
-                        if (view.getUint16(offset + (i * 12), little) == 0x0112) {
+                        if (view.getUint16(offset + (i * 12), little) === 0x0112) {
                             return callback(view.getUint16(offset + (i * 12) + 8, little));
                         }
                     }
